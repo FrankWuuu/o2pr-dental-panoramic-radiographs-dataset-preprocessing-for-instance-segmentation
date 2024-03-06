d73663f210f1f468c82e5ff448b0178fe9e419ee
FrankWu <frankwu2029@gmail.com>
2024-03-06 20:35:12 +0800

    Create count_filename_in_json.py

---
+++ count_filename_in_json.py
@@ -0,0 +1,22 @@
+import json
+
+# json_name = 'train.json'
+# csv_dst = 'train_lists.csv'
+
+json_name = 'test.json'
+csv_dst = 'test_lists.csv'
+
+with open(json_name) as json_file:
+    labelbox_data = json.load(json_file)
+
+file_names = []
+
+print(len(labelbox_data['images']))
+
+for image in labelbox_data['images']:
+    file_name = image['file_name']
+    file_names.append(file_name)
+
+with open(csv_dst,'w') as f:
+    for r in file_names:
+        f.write(r+"\n")
